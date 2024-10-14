5598c4502a26584a75e87463a0a394934442557f
SuperLlama88888 <116475184+SuperLlama88888@users.noreply.github.com>
2024-10-14 02:06:09 +0000

    pls

--- src/main/java/audaki/cart_engine/AudakiCartEngine.java
+++ src/main/java/audaki/cart_engine/AudakiCartEngine.java
@@ -1,19 +1,19 @@
 package audaki.cart_engine;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.entity.vehicle.AbstractMinecartEntity;
+import net.minecraft.entity.vehicle.AbstractMinecart;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
 public class AudakiCartEngine implements ModInitializer {
-    public static List<Function<AbstractMinecartEntity, Boolean>> CART_CHECK_MODIFIED_ENGINE = new ArrayList<>();
+    public static List<Function<AbstractMinecart, Boolean>> CART_CHECK_MODIFIED_ENGINE = new ArrayList<>();
 
-    // Function should take one AbstractMinecartEntity. Return true to force
+    // Function should take one AbstractMinecart. Return true to force
     // modified speed logic, false to force vanilla speed logic, and null
     // otherwise.
-    public static void registerModifiedEngineCheck(Function<AbstractMinecartEntity, Boolean> cartChecker) {
+    public static void registerModifiedEngineCheck(Function<AbstractMinecart, Boolean> cartChecker) {
         CART_CHECK_MODIFIED_ENGINE.add(cartChecker);
     }
 
